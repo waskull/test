51a23ad4469dd218dc265603dd8ff0ffc6bc7dd1
Martin <mrtncsto@gmail.com>
2025-11-14 20:03:51 -0400

    agregando radio

--- prueba.py
+++ prueba.py
@@ -1,10 +1,13 @@
 class Auto:
-	def __init__(self, placa, modelo, color):
+	def __init__(self, placa, modelo, color, radio):
 		self.placa = placa
 		self.modelo = modelo
 		self.color = color
+		self.radio = radio
 	def mostrar_datos(self):
 		print(f"El modelo del auto es {modelo}, su placa es {placa} y su color es: {color}")
+		if radio: print("Tiene Radio")
+		else: print("No Tiene Radio")
 
-auto = Auto()
+auto = Auto("5345-4324","Escarabajo","Blanco",True)
 auto.mostrar_datos()
